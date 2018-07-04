5e4638fc366c789f8a210ae1651c87e5b5a4554e
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2018-07-05 00:32:17 +0600

    Addition

--- BasicIO/Addition.asm
+++ BasicIO/Addition.asm
@@ -1,57 +1,54 @@
 .MODEL SMALL
-
+.STACK 100H
 
 .DATA
-  
+
   MSG1 DB 'ENTER TWO NUMBERS : $'
   MSG2 DB 'THE SUM IS : $'
-  
+
   X DB 5
   Y DB 4
-  
+
 .CODE
 MAIN PROC
-   
+
    MOV AX , @DATA
    MOV DS, AX
-   
+
    LEA DX , MSG1
-   MOV AH , 9 
+   MOV AH , 9
    INT 21H
-   
+
    MOV AH , 1
    INT 21H
    MOV BL , AL
-   
+
    MOV AH , 1
    INT 21H
-   
+
    SUB AL , 48
    SUB BL , 48
-   
+
    ADD BL , AL
    ADD BL , 48
-   
+
    LEA DX , MSG2
-   MOV AH , 9 
+   MOV AH , 9
    INT 21H
-   
+
    ;MOV CH , X
    ;ADD CH , Y
    ;ADD CH , 48
-   
-   
+
+
    MOV AH , 2
    MOV DL , BL
    INT 21H
-   
+
    MOV AH , 4CH
    INT 21H
-    
-    
+
+
 MAIN ENDP
 
 END MAIN
-
-
-
